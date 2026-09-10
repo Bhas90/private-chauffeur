a58d043ebd636b932fb32fd200d20a608152ba60
Bhaskar Reddy <164631537+Bhas90@users.noreply.github.com>
2026-09-10 17:48:35 +0530

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { FleetModule } from "./fleet/fleet.module";
 import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
@@ -8,8 +7,10 @@ import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { BlogsModule } from "./blogs/blogs.module";
 import { DatabaseModule } from "./database/database.module";
+import { FleetModule } from "./fleet/fleet.module";
 import { MailModule } from "./mail/mail.module";
 import { MailSettingsModule } from "./mail-settings/mail-settings.module";
+import { SitemapModule } from "./sitemap/sitemap.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
@@ -30,7 +31,9 @@ import { UsersModule } from "./users/users.module";
 
     MailModule,
 
-     FleetModule,
+    FleetModule,
+
+    SitemapModule,
   ],
 
   controllers: [
